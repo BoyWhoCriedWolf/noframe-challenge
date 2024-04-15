995253633ec549fc39aabb928fa2b70709ca76b1
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-04-15 20:53:27 +0800

    feat: project skeleton

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,19 @@
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <Stack sx={{ minHeight: "100vh" }} justifyContent={"space-between"}>
+      <Header />
+      <Box flexGrow={1}>
+        <Outlet />
+      </Box>
+      <Footer />
+    </Stack>
+  );
+};
+
+export default Layout;
