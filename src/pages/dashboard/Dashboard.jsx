c7ec4401e4c5c777e2cd2e3fba363ae5c6efeba5
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-04-16 13:49:29 +0800

    fix: for deploy

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,8 @@
+import { Container } from "@mui/material";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <Container>Dashboard</Container>;
 };
 
 export default Dashboard;
